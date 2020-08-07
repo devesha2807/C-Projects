f53daa565b112c271010a14db41ee479864afeb6
devesha2807 <deveshpaliwalrocks@gmail.com>
2020-08-07 17:46:27 +0530

    Add files via upload

---
+++ Bubble Sorting in C.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<string.h>
+#include<math.h>
+#include<time.h>
+
+
+
+
+int main()
+{
+int i, temp, swapped;
+int howMany = 10;
+int goals[howMany];
+srand(time(NULL));
+
+
+for(i = 0; i<howMany; i++){
+
+goals[i] = ( rand()%25 ) + 1;
+}
+
+printf("Original List of Elemtents\n");
+for(i = 0; i<howMany; i++){
+printf("%d \n", goals[i]);
+
+}
+while(1){
+
+swapped = 0;
+
+for(i=0; i<howMany-1; i++){
+
+if(goals[i] > goals[i+1]){
+
+int temp = goals[i];
+goals[i] = goals[i + 1];
+goals[i+1] = temp;
+
+swapped = 1;
+
+}
+
+}
+
+if(swapped == 0){
+break;
+}
+}
+
+
+
+printf("\nSorted or Arranged List\n");
+for(i=0; i <howMany; i++){
+printf("%d \n", goals[i]);
+
+}
+
+return 0;
+
+}
